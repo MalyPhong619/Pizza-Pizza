ed18dbedf02380ee2af56fd8dc5d3c3560899d20
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-11-02 11:25:55 -0700

    submit pizzaOrderForm, define protein from input as var pizzaProtein

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 
     var pizzaSize = parseInt($("input:radio[name=pizzaSize]:checked").val());
     console.log(pizzaSize);
+    var pizzaProtein = parseInt($("input:radio[name=protein]:checked").val());
+    console.log(pizzaProtein);
 
   });
 
