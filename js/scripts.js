bc8b7976c64b4b2929c9bb3b000ac66efc849bf6
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-11-02 11:58:21 -0700

    create prototype for pizza cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+function Add(a, b, c) {
+  return a + b + c;
+}
+
 function Pizza(size, protein, topping) {
-  this.size = pizza,
+  this.size = size,
   this.protein = protein,
   this.topping = topping,
   this.startPrice = 5
 }
+
+Pizza.prototype.Cost = function() {
+  this.startPrice += Add(this.size, this.protein, this.topping);
+  $(".completedOrder").text(this.startPrice);
+}
+
+
 
 
 
@@ -16,7 +27,11 @@ $(document).ready(function() {
     var pizzaProtein = parseInt($("input:radio[name=protein]:checked").val());
     var pizzaToppings = parseInt($("input:checkbox[name=toppings]:checked").val());
 
+    var pizza = new Pizza(pizzaSize, pizzaProtein, pizzaToppings);
 
+    console.log(pizza);
+
+    pizza.Cost();
 
 
   });
